b401e5948dc216473b7af5ed3cc34fbbc126c251
Andrew Doyle <andrew.doyle.83@gmail.com>
2016-02-29 22:04:32 +1100

    merge conflict :

--- src/Game/Command/HelpCommand.php
+++ src/Game/Command/HelpCommand.php
@@ -23,6 +23,7 @@ class HelpCommand extends Command
         $help_msg .= " |_ Seer - A villager who, once each night, is allowed to see the role of another player. The bot will private message you.\r\n";
         $help_msg .= " |_ Tanner - A player not on the side of the villagers or the werewolves who wins if is killed.\r\n";
         $help_msg .= " |_ Lycan - A villager who appears to the Seer as a Werewolf.\r\n";
+        $help_msg .= " |_ Wolfman - A Werewolf who appears to the Seer as a villager.\r\n";
         $help_msg .= " |_ Beholder - A villager who learns who the Seer is on the first night\r\n";
         $help_msg .= " |_ Bodyguard - A villager who may protect a player from being eliminated once each night, but not the same person two nights in a row.\r\n\r\n";
         $help_msg .= " |_ Minion - A minion is on the side of the werewolves, and gets to see who the werewolves are, but they don't know who he is.\r\n";
