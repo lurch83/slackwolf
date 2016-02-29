4c2d78b48697a2985cb8c58e5b450143cac52653
Nick Ellercamp <n.ellercamp@nib.com.au>
2016-03-01 09:51:57 +1100

    Show the minion as a villager, not a werewolf

--- src/Game/Command/SeeCommand.php
+++ src/Game/Command/SeeCommand.php
@@ -167,7 +167,7 @@ class SeeCommand extends Command
                 continue;
             }
 
-            if ($player->role == Role::WEREWOLF || $player->role == Role::LYCAN || $player->role == Role::MINION) {
+            if ($player->role == Role::WEREWOLF || $player->role == Role::LYCAN) {
                 $msg = "@{$player->getUsername()} is on the side of the Werewolves.";
             } else {
                 $msg = "@{$player->getUsername()} is on the side of the Villagers.";
