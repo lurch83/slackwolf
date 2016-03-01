b0507c26452a5c758ff0a82c6a698b0894d45106
Andrew Doyle <andrew.doyle.83@gmail.com>
2016-03-01 21:59:46 +1100

    allow dev param to check for .env.dev for testing purposes

--- bot.php
+++ bot.php
@@ -18,7 +18,7 @@ require __DIR__.'/vendor/autoload.php';
 /*
 * Load dotenv to be able to access .env configuration variables
 */
-$dotenv = new Dotenv(__DIR__);
+$dotenv = new Dotenv(__DIR__, (isset($argv[1]) && $argv[1] == "dev") ? '.env.dev' : '.env');
 $dotenv->load();
 
 error_reporting(E_ALL);
